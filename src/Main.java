7f9af99926d7a898133b01f32cff671965d65f35
Baran Öztürk <ozturkb2006@gmail.com>
2026-03-20 01:35:11 +0300

    All arrays were created for using at the future steps

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
         int[] randomArray = FileOperations.loadFile_generateArray("random.txt");
         int[] semi_orderedArray = FileOperations.loadFile_generateArray("semi_ordered.txt");
+        int[] increasingArray = new int[1000000];
+        int[] decreasingArray = new int[1000000];
+
+
+        for(int i = 0; i < increasingArray.length; i++){
+            increasingArray[i] = i;
+            decreasingArray[decreasingArray.length-i] = i;
+        }
 
     }
 }
